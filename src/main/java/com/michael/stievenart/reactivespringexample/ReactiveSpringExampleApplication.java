33a1b91ab75e704f591cd363e90a881b60f6a15a
michaelstievenart <michael.l.stievenart@gmail.com>
2018-10-31 11:29:33 +0200

    Basic Spring boot project

---
+++ src/main/java/com/michael/stievenart/reactivespringexample/ReactiveSpringExampleApplication.java
@@ -0,0 +1,12 @@
+package com.michael.stievenart.reactivespringexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveSpringExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveSpringExampleApplication.class, args);
+	}
+}
